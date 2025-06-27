894078009f1deaba86cf2284b8d9dd8883a908c1
Burak Sayan <buraksayan@Burak-MacBook-Pro.local>
2025-06-27 16:38:50 +0300

    User schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,28 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-}, { timestamps: true });
+  password: { type: String, required: true, unique: true },
+  city: { type: String, required: false },
+  createdAt: { type: Date, default: Date.now },
+  likedMovies: [
+    {
+      movieId: { type: String, required: true },
+      likedAt: { type: Date, default: Date.now }
+    }
+  ],
+  watchlist: [
+    {
+      movieId: { type: String, required: true },
+      addedAt: { type: Date, default: Date.now }
+    }
+  ],
+  comments: [
+    {
+      movieId: { type: String, required: true },
+      commentText: { type: String },
+      commentedAt: { type: Date, default: Date.now }
+    }
+  ]
+});
 
 module.exports = mongoose.model('User', userSchema); 
